6b9483a85c343fcac0c958b35e843ac40f8b9256
cabbit0x11 <luarennix@gmail.com>
2026-08-19 16:18:44 +0300

    done

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
-    int w, h;
-    scanf("%d", &w);
-    scanf("%d", &h);
-    printf("%d\n",h*w);
+int main(void)
+{
+    int nums[5];
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+
+
+
+    int biggest = nums[0];
+    for (int i = 1; i < 5; i++)
+    {
+        if (nums[i] > biggest) {
+            biggest = nums[i];
+        }
+    }
+    printf("%d\n", biggest);
     return 0;
 }
