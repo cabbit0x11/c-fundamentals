5d1172b9ace8e0570e632fd03b15dc7df33942c2
cabbit0x11 <luarennix@gmail.com>
2026-08-20 15:06:22 +0300

    done

--- main.c
+++ main.c
@@ -3,18 +3,16 @@
 
 int main(void) {
     char buf[100];
-
-    if (fgets(buf, sizeof buf, stdin) == NULL) {
-        return 1;
-    }
-
+    fgets(buf, sizeof buf, stdin);
+    
     for (int i = 0; buf[i] != '\0'; i++) {
         if (buf[i] == '\n') {
             buf[i] = '\0';
             break;
         }
     }
-    printf("%zu\n", strlen(buf));
-
+    
+    printf("%d\n", (int)strlen(buf));
+    
     return 0;
 }
