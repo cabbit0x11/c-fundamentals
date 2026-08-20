56eba5cb77a8af5e645f2693b3850b8962745582
cabbit0x11 <luarennix@gmail.com>
2026-08-20 15:31:40 +0300

    done

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-int main(void)
-{
+int square(int n) {
+    return n*n;
+}
+
+int main(void) {
     int n;
-    int sum = 0;
     scanf("%d", &n);
-    for (int i = 0; i <= n; i++)
-    {
-        sum = sum + i;
-    }
-    printf("%d", sum);
+    printf("%d\n", square(n));
     return 0;
 }
